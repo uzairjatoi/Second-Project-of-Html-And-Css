62d877a876fa98cba821ee154a9e702771aa1b24
uzairjatoi <116812323+uzairjatoi@users.noreply.github.com>
2023-05-13 15:46:57 +0500

    Add files via upload

---
+++ second project of html and css/index.js
@@ -0,0 +1,28 @@
+AOS.init({
+    duration: 1200,
+  })
+
+  $(window).on("load resize",function() {
+
+    var counters = $(".count");
+    var countersQuantity = counters.length;
+    var counter = [];
+  
+    for (i = 0; i < countersQuantity; i++) {
+      counter[i] = parseInt(counters[i].innerHTML);
+    }
+  
+    var count = function(start, value, id) {
+      var localStart = start;
+      setInterval(function() {
+        if (localStart < value) {
+          localStart++;
+          counters[id].innerHTML = localStart;
+        }
+      }, 4);
+    }
+  
+    for (j = 0; j < countersQuantity; j++) {
+      count(0, counter[j], j);
+    }
+  });
